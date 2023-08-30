409ddce634087c0a31e661159775871247baf434
joshuasemana <joshuadrummer14@gmail.com>
2023-08-30 22:30:17 +0800

    Animation on Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,31 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    container: {
-      center: true,
+    extend: {
+      animation: {
+        blob: "blob 8s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            opacity: 0.4,
+            transform: "scale(1) translate(0px, 0px)",
+          },
+          "33%": {
+            opacity: 0.3,
+            transform: "scale(1.1) translate(30px, -50px)",
+          },
+          "66%": {
+            opacity: 0.2,
+            transform: "scale(0.9) translate(-20px, 20px)",
+          },
+          "100%": {
+            opacity: 0.4,
+            transform: "scale(1) translate(0px, 0px)",
+          },
+        },
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
